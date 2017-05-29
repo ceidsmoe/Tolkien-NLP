6ad8817749fb7993a828544862de122e979c7218
ceidsmoe <ceidsmoe@uchicago.edu>
2017-05-28 23:17:41 -0500

    fixed frequency.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tolkien-NLP
+
+I've included in this file most of what you'll need to reproduce my results. Obviously due to copyright law you won't find the full text of Lord of the Rings here, but the text file of phonemes should suffice to run the remaining code. You should be able to just run euphony.py and generate the necessary coefficients.
+
+From there, the included Jupyter notebook should suffice to parse through the output and see how I arrived at my results.
+
+If you would like to 
--- frequency.py
+++ frequency.py
@@ -5,34 +5,19 @@ import json
 import re
 from StringIO import StringIO
 
-words = open("phoneme_output.txt", "r").readlines()
+sentences = open("phonemes.txt", "r").readlines()
 
-total = len(words)
 frequencies = {}
+total = 0
 
-
-g2p_model = G2PModel('g2p-seq2seq-cmudict')
-g2p_model.load_decode_model()
-
-M = {}
-
-for word in words:
-	for ph in word.split()[1:]:
-		if len(ph) > 2:
-			sub_phs = M.get(ph, [])
-
-			if not sub_phs:
-				sub_phs = g2p_model.decode([ph], StringIO())
-				M[ph] = sub_phs
-
-			for sph in sub_phs[0].split():
-				frequencies[sph] = frequencies.get(sph, 0) + 1
-		else:
-			frequencies[ph] = frequencies.get(ph, 0) + 1
+for sentence in sentences:
+	for ph in sentence.split():
+		total += 1
+		frequencies[ph] = frequencies.get(ph, 0) + 1
 
 for k, v in frequencies.iteritems():
 	frequencies[k] = float(v) / total
 
 output = open("phfreq.json", "w")
 
-output.write(json.dump(frequencies))
+output.write(json.dumps(frequencies))
